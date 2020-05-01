f16f746641e368c4d652e0fd65c0197e1fe7ade4
Kelifa, Alexandre <alexandre.kelifa@hotmail.com>
2020-05-01 04:38:35 +0200

    models City & Specialties added + relations

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,19 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
 
+City.create(name: "Prefailles")
+City.create(name: "Pornic")
+City.create(name: "Nantes")
+puts "We have created 3 cities"
+
+Specialty.create(name: "Musicothérapeuthe")
+Specialty.create(name: "Artothérapeuthe")
+Specialty.create(name: "Thérapeuthe du CODE")
+puts "We have created 3 specialties"
+
 Doctor.create(first_name: "Jacky", last_name:"Galou", zip_code:"44770")#, specialty:"musicotherapeuthe": La colonne specialty a été retirée grace à une migration
 Doctor.create(first_name: "Johnny", last_name:"Vaconces", zip_code:"75000")#, specialty:"musicotherapeuthe"
-puts "two Doctors were created"
+puts "Two Doctors were created"
 
 Patient.create(first_name: "Emmanuel", last_name:"Macron")
 Patient.create(first_name: "Sylvestre", last_name:"Standalone")
@@ -20,7 +30,3 @@ Appointment.create(doctor:Doctor.find(2), patient:Patient.find(2), date:Date.tod
 Appointment.create(doctor:Doctor.find(1), patient:Patient.find(3), date:Date.today)
 puts "Two appointments were scheduled"
 puts "You can verify that the column specialty was erased, in the rails console by typing Doctors.all"
-
-
-
-
